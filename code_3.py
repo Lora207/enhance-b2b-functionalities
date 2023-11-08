c49e5d497235bce96155168b013707e9f358ff09
Lora <143578889+Lora207@users.noreply.github.com>
2023-11-08 21:54:33 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,39 @@
+    return data
+def main():
+
+    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+        print(f"Random Number: {item}")
+
+import random
+    main()
+def generate_random_data():
+
+if __name__ == "__main__":import random
+
+
+    main()
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+def main():
+
+def generate_random_data():    data = generate_random_data()
+
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+import random
+def main():
+    for item in data:
+if __name__ == "__main__":
+    main()
+    return data
