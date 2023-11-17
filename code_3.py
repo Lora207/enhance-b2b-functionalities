da813c78c3bdbbbf22da8856ab72f8b0324d0390
Lora <143578889+Lora207@users.noreply.github.com>
2023-11-17 07:32:19 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,39 +1,57 @@
-    return data
-def main():
+        int[] data = new int[10];
+        }
 
-    data = generate_random_data()
+        for (int i = 0; i < 10; i++) {
+        }
+}
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+public class RandomDataGenerator {
 
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-        print(f"Random Number: {item}")
+    public static void main(String[] args) {def generate_random_data():
 
-import random
-    main()
-def generate_random_data():
-
-if __name__ == "__main__":import random
-
-
-    main()
-if __name__ == "__main__":
         print(f"Random Number: {item}")
     data = generate_random_data()
-
-    return data
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
 def main():
-
-def generate_random_data():    data = generate_random_data()
-
-
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-
 import random
-def main():
+
     for item in data:
 if __name__ == "__main__":
-    main()
+
+
     return data
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()    for (let i = 0; i < 10; i++) {
+main();
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];
+    const data = generateRandomData();
+
+
+function main() {
+}
+}
+    return data;
+    }
+function generateRandomData() {
+
+    });
+        console.log("Random Number: " + item);        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+    });
+main();
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
+
+function main() {
+}
+    }
+    const data = [];
+}
+
+    return data;
+function generateRandomData() {
