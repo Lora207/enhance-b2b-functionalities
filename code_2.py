0e88f686aedd1f7b807c8b99d4918c87b0755dcd
Lora <143578889+Lora207@users.noreply.github.com>
2023-11-25 17:13:36 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,42 +1,56 @@
-    return data
-def main():
-    for item in data:
-
-if __name__ == "__main__":
 
 
-        print(f"Random Number: {item}")
-def generate_random_data():
-    main()
 
 import random
+def generate_random_data():
     data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]    return data;
+        print(f"Random Number: {item}")
+    return data
+    for item in data:
+    main()
+if __name__ == "__main__":
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
 
+
+def main():
+
+    return data
+
+    main()
+def generate_random_data():
+    data = generate_random_data()
+import random
+        print(f"Random Number: {item}")
+if __name__ == "__main__":function main() {
     const data = [];
-}
-function main() {
-main();
-        data.push(Math.floor(Math.random() * 100) + 1);
-    }
-    const data = generateRandomData();
-    for (let i = 0; i < 10; i++) {
-function generateRandomData() {
 
-    data.forEach(item => {
 }
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+    }
+main();
 
     });
-        console.log("Random Number: " + item);def main():
+    return data;
+    data.forEach(item => {
+}
+function generateRandomData() {
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+import random
+        print(f"Random Number: {item}")
+def main():
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
 if __name__ == "__main__":
 
-    data = [random.randint(1, 100) for _ in range(10)]
 
-    data = generate_random_data()
     return data
 def generate_random_data():
-        print(f"Random Number: {item}")
-import random
-    for item in data:
 
     main()
